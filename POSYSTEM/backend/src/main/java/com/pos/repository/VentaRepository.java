04ea3c2c521fb533062e5b4ae4aa03f65644cb6e
julian bolanos <jbolanos@unimayor.edu.co>
2026-09-20 14:12:19 -0500

    "Configuracion Git"

--- POSYSTEM/backend/src/main/java/com/pos/repository/VentaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.pos.repository;
-
-import com.pos.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Integer> {}
